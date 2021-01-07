741b3a420bea422c7ea9d736d90ba70d4c7730a2
nayeon <vennie20@naver.com>
2021-01-07 17:34:26 +0900

    210107

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,12 +6,11 @@
 /*   By: naykim <naykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/31 04:10:32 by naykim            #+#    #+#             */
-/*   Updated: 2021/01/07 17:31:17 by naykim           ###   ########.fr       */
+/*   Updated: 2021/01/07 17:34:05 by naykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 void				*ft_memcpy(void *dest, const void *src, size_t num)
 {
@@ -19,7 +18,7 @@ void				*ft_memcpy(void *dest, const void *src, size_t num)
 	unsigned char	*str2;
 	size_t			i;
 	
-	if (src == 0)
+	if (src[0] == '\0')
 		return (NULL);
 	str1 = (unsigned char *)dest;
 	str2 = (unsigned char *)src;
@@ -29,10 +28,4 @@ void				*ft_memcpy(void *dest, const void *src, size_t num)
 	while (num--)
 		*(str1++) = *(str2++);
 	return (dest);
-}
-
-int main()
-{
-	printf("%s", ft_memcpy((void *)0, (void *)0, 3));
-	return 0;
 }
