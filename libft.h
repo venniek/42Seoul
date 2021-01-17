1a2f12e066eb6721f8cabd32dc186b9825c7d9fb
nayeon <vennie20@naver.com>
2021-01-17 11:49:27 +0900

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: naykim <naykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/02 20:24:56 by naykim            #+#    #+#             */
-/*   Updated: 2021/01/15 16:14:09 by naykim           ###   ########.fr       */
+/*   Updated: 2021/01/17 11:46:49 by naykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,5 +65,7 @@ t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *new);
 void				ft_lstdelone(t_list *lst, void (*del)(void *));
 void				ft_lstclear(t_list **lst, void (*del)(void *));
+void				ft_lstiter(t_list *lst, void (*f)(void *));
+t_list				*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
