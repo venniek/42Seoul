e5c20c5f6963687c4cdb20f17abc29da0b9c9486
Nayeon Kim <naykim@c2r4s1.42seoul.kr>
2021-01-20 13:54:54 +0900

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: naykim <naykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/30 02:55:44 by naykim            #+#    #+#             */
-/*   Updated: 2021/01/05 18:06:38 by naykim           ###   ########.fr       */
+/*   Updated: 2021/01/20 13:53:46 by naykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ size_t		ft_strlcat(char *dst, const char *src, size_t size)
 	k = 0;
 	while (dst[i] && i < size)
 		i++;
-	if (i >= size)
-		return (size + ft_strlen(src));
 	while (i + k + 1 < size && src[k])
 	{
 		dst[i + k] = src[k];
