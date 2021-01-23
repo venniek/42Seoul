f8f4b3542bffccc027341e90f8bc69a273f75290
nayeon <vennie20@naver.com>
2021-01-23 16:57:51 +0900

    ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,33 +5,68 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: naykim <naykim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/04 17:24:53 by naykim            #+#    #+#             */
-/*   Updated: 2021/01/09 18:51:42 by naykim           ###   ########.fr       */
+/*   Created: 2021/01/23 15:14:12 by naykim            #+#    #+#             */
+/*   Updated: 2021/01/23 16:55:12 by naykim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		*ft_itoa(int n)
+static int	ft_len(int c)
 {
-	char	*str;
+	int		len;
 
-	if (!(str = (char *)malloc(sizeof(char) * 2)))
+	len = 0;
+	if (c < 0)
+	{
+		len++;
+		c *= -1;
+	}
+	while (c > 0)
+	{
+		len++;
+		c /= 10;
+	}
+	return (len);
+}
+
+static char	*make_itoa(char *s, int c, int len)
+{
+	int		i;
+
+	if (c < 0)
+	{
+		c *= -1;
+		s[0] = '-';
+	}
+	i = 0;
+	while (c > 0)
+	{
+		s[len - i - 1] = c % 10 + '0';
+		c /= 10;
+		i++;
+	}
+	s[len] = '\0';
+	return (s);
+}
+
+char		*ft_itoa(int c)
+{
+	char	*s;
+	int		len;
+	if (c == -2147483648)
+		return (s = ft_strdup("-2147483648"));
+	if (c == 0)
+	{
+		if (!(s = (char *)malloc(sizeof(char) * 2)))
+			return (NULL);
+		s[0] = '0';
+		s[1] = '\0';
+		return (s);
+	}
+	len = ft_len(c);
+	if (!(s = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	if (n == -2147483648)
-		return (ft_strdup("-2147483648"));
-	if (n < 0)
-	{
-		str[0] = '-';
-		str[1] = '\0';
-		str = ft_strjoin(str, ft_itoa(-n));
-	}
-	else if (n >= 10)
-		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
-	else
-	{
-		str[0] = n + '0';
-		str[1] = '\0';
-	}
-	return (str);
+	make_itoa(s, c, len);
+	return (s);
 }
