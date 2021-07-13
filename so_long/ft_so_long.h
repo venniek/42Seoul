043b27f1a2518611f82b83ad16fd24d4cfb7f849
nayeon <vennie20@naver.com>
2021-07-13 17:42:24 +0900

    map, img

---
+++ so_long/ft_so_long.h
@@ -0,0 +1,19 @@
+#ifndef FT_SO_LONG_H
+# define FT_SO_LONG_H
+
+# include <mlx.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+# define K_ESC 53
+# define K_LEFT 0
+# define K_RIGHT 2
+# define K_UP 13
+# define K_DOWN 1
+
+void make_map(char *av[]);
+
+
+
+#endif
--- so_long/main.c
+++ so_long/main.c
@@ -1,33 +1,75 @@
 #include <mlx.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-typedef struct s_data{
+typedef struct s_img{
 	void *img;
 	char *addr;
-	int bits_per_pixel;
+	int width;
+	int height;
+	int bpp;
 	int line_length;
 	int endian;
-}	t_data;
+}	t_img;
 
-void my_mlx_pixel_put(t_data *data, int x, int y, int color)
+typedef struct s_vars {
+	void *mlx;
+	void *win;
+} t_vars;
+
+void my_mlx_pixel_put(t_img *data, int x, int y, int color)
 {
 	char *dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->addr + (y * data->line_length + x * (data->bpp / 8));
 	*(unsigned int*)dst = color;
+}
+
+int close(int keycode, t_vars *vars)
+{
+	if (keycode == 53)
+	{	
+		exit(0);
+	}
+	return 0;
 }
 
 int main(void)
 {
-	void *mlx;
-	void *mlx_win;
-	t_data img;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 500, 500, "start");
-	img.img = mlx_new_image(mlx, 500, 500);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, 250, 250, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	t_vars vars;
+	t_img img;
+/*
+	if (ac == 1)
+	{
+		printf("Error\n");
+		printf("no map\n");
+		exit(1);
+	}
+	if (ac > 2)
+	{
+		printf("Error\ntoo much map\n");
+		exit(1);
+	}
+	*/
+//	make_map(av);
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 500, 500, "start");
+	img.img = mlx_xpm_file_to_image(vars.mlx, "./imgs/sample.xpm", &(img.width), &(img.height));
+	img.addr = mlx_get_data_addr(img.img, &(img.bpp), &(img.line_length), &(img.endian));
+	int y = 0;
+	while (y < img.height)
+	{
+		int x = 0;
+		while (x < img.width)
+		{
+			if (x % 10 == 0)
+				img.addr[img.width * y + x] = (char)0x00FF00;   // ?????
+			x++;
+		}
+		y++;
+	}
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
+	mlx_loop(vars.mlx);
 	return 0;
 }
