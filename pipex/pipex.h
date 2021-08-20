a8ea3aa35f43fa81fe59a3f7bae8d8ced337e0dc
nayeon <vennie20@naver.com>
2021-08-20 14:33:01 +0900

    px first makefile n header

---
+++ pipex/pipex.h
@@ -0,0 +1,12 @@
+#IFNDEF PIPEX_H
+#DEFINE PIPEX_H
+
+# include <unistd.h>   //access unlink close read fork pipe dup dup2 execve
+# include <stdlib.h>   //malloc free exit
+# include <fcntl.h>    // open write
+# include <sys/wait.h> // wait waitpid
+# include <stdio.h>    // perror
+# include <string.h>   // strerror
+
+
+#endif
