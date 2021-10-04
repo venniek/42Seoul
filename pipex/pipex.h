fb22a3267a06a59726910b4351bcc9bf1f27c2f7
Nayeon Kim <naykim@c2r4s1.42seoul.kr>
2021-10-04 17:13:10 +0900

    px header n include

--- pipex/pipex.h
+++ pipex/pipex.h
@@ -13,13 +13,11 @@
 #ifndef PIPEX_H
 # define PIPEX_H
 
-# include <unistd.h>   //access unlink close read fork pipe dup dup2 execve
-# include <stdlib.h>   //malloc free exit
-# include <fcntl.h>    // open write
-# include <sys/wait.h> // wait waitpid
-# include <stdio.h>    // perror
-# include <string.h>   // strerror
-# include <errno.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <stdio.h>
 # include "libft.h"
 
 typedef struct s_var
