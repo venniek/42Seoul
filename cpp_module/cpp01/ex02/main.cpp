51341a9670c99176a298c5865c946f9f498d8aa5
Nayeon Kim <naykim@c2r4s1.42seoul.kr>
2022-02-18 19:45:51 +0900

    cpp 01-02 edit

--- cpp_module/cpp01/ex02/main.cpp
+++ cpp_module/cpp01/ex02/main.cpp
@@ -7,10 +7,11 @@ int main()
 	std::string* strPTR = &str;
 	std::string& strREF = str;
 
-	std::cout << "str: " << &str << std::endl;
-	std::cout << "by strPTR: " << strPTR << std::endl;
-	std::cout << "by &strREF: " << &strREF << std::endl << std::endl;
+	std::cout << "address of str: " << &str << std::endl;
+	std::cout << "     by strPTR: " << strPTR << std::endl;
+	std::cout << "    by &strREF: " << &strREF << std::endl << std::endl;
 	
+	std::cout << "str          : " << str << std::endl;
 	std::cout << "str using PTR: " << *strPTR << std::endl;
 	std::cout << "str using REF: " << strREF << std::endl;
 
