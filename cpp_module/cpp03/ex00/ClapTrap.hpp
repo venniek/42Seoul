3679900313298889bdf602c2fd0e00e1f4dac7d2
nayeon <vennie20@naver.com>
2022-02-27 18:02:23 +0900

    cpp 03-00 done

--- cpp_module/cpp03/ex00/ClapTrap.hpp
+++ cpp_module/cpp03/ex00/ClapTrap.hpp
@@ -3,20 +3,38 @@
 
 #include <string>
 #include <iostream>
+#include <iomanip>
 
 class ClapTrap {
 private:
-	std::string name;
-	unsigned int hit;
-	unsigned int energy;
-	unsigned int attackDamage;
+	std::string _name;
+	unsigned int _hit;
+	unsigned int _ep;
+	unsigned int _ad;
+
+	const unsigned int _initHit;
 
 public:
-	ClapTrap(std::string name);
 	void attack(std::string const &target);
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
+
+	std::string getName() const;
+	unsigned int getHit() const;
+	unsigned int getEp() const;
+	unsigned int getAd() const;
+	unsigned int getInitHit() const;
+
+	void setName(const std::string &name);
+	void setHit(unsigned int hit);
+	void setEp(unsigned int ep);
+	void setAd(unsigned int ad);
+
 	void printStatus();
+
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap& origin);
+	ClapTrap& operator=(const ClapTrap& origin);
 	~ClapTrap();
 };
 
--- cpp_module/cpp03/ex00/main.cpp
+++ cpp_module/cpp03/ex00/main.cpp
@@ -1,18 +1,25 @@
 #include "ClapTrap.hpp"
 
 int main() {
-	ClapTrap clap("naykim");
+	ClapTrap clap("A");
+	ClapTrap clap2(clap);
 
 	clap.printStatus();
-	clap.attack("mikyan");
-	clap.beRepaired(10);
-	clap.printStatus();
-	clap.beRepaired(45);
-	clap.printStatus();
+
+	clap.attack("B");
 	clap.takeDamage(10);
 	clap.printStatus();
+
 	clap.takeDamage(45);
 	clap.printStatus();
+
+	clap.beRepaired(10);
+	clap.printStatus();
+
+	clap.beRepaired(45);
+	clap.printStatus();
+
+	clap2.printStatus();
 	
 	return 0;
 }
