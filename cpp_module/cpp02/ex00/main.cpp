73fdf9c390812e22cf58cd786a0ce02ad8b0278d
Nayeon Kim <naykim@c2r4s1.42seoul.kr>
2022-03-04 18:29:14 +0900

    cpp 02 edit

--- cpp_module/cpp02/ex00/main.cpp
+++ cpp_module/cpp02/ex00/main.cpp
@@ -15,6 +15,7 @@ int main() {
 	std::cout << std::endl << "after a.setRawBits(100)" << std::endl;
 	std::cout << "a.getRawBits: " << a.getRawBits() << std::endl;
 	std::cout << "b.getRawBits: " << b.getRawBits() << std::endl;
+	std::cout << "c.getRawBits: " << c.getRawBits() << std::endl;
 
 	return 0;
 }
--- cpp_module/cpp02/ex02/main.cpp
+++ cpp_module/cpp02/ex02/main.cpp
@@ -37,7 +37,7 @@ int main() {
 	std::cout << "b: " << b << std::endl;
 	std::cout << "a + b ? " << a + b << std::endl;
 	std::cout << "a - b ? " << a - b << std::endl;
-	std::cout << "a * b ? " << a * b<< std::endl;
+	std::cout << "a * b ? " << a * b << std::endl;
 	std::cout << "a / b ? " << a / b << std::endl;
 
 	return 0;
