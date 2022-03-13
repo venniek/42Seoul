85b37fa7e566bf8daff9ba280f2e4d10afb695ef
nayeon <vennie20@naver.com>
2022-03-13 17:08:48 +0900

    cpp 07-01 done

---
+++ cpp_module/cpp07/ex01/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "iter.hpp"
+
+template<typename T>
+void display( T &whatever ) {
+	std::cout << "value is: " << whatever << std::endl;
+}
+
+template<typename T>
+void plusFive(T& whatever) {
+	whatever += 5;
+}
+
+int	main( void ) {
+	std::string	strTable[3];
+	int			*intTable = new int[5];
+
+	strTable[0] = "Hi how are you?";
+	strTable[1] = "Fine, and you dear sir?";
+	strTable[2] = "Very fine indeed!";
+	iter(strTable, 3, &display);
+	std::cout << std::endl;
+	for (int i = 0; i < 5; i ++)
+		intTable[i] = i * 3 + 7;
+	iter(intTable, 5, &display);
+	iter(intTable, 5, &plusFive);
+	std::cout << std::endl;
+	iter(intTable, 5, &display);
+}
