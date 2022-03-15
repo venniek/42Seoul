b0b71500983f36bd43425ef70806003da9d997a2
Nayeon Kim <naykim@c2r4s1.42seoul.kr>
2022-03-15 21:59:41 +0900

    cpp 06-01 done

---
+++ cpp_module/cpp06/ex01/main.cpp
@@ -0,0 +1,24 @@
+#include "Data.hpp"
+#include <iostream>
+#include <stdint.h>
+
+uintptr_t serialize(Data* ptr) {
+	return reinterpret_cast<uintptr_t>(ptr);
+}
+
+Data* deserialize(uintptr_t raw)  {
+	return reinterpret_cast<Data *>(raw);
+}
+
+int main() {
+	Data		*ptr = new Data;
+	Data		*newPtr;
+	uintptr_t	raw;
+
+	ptr->data = "hello world 13!@#";
+	raw = serialize(ptr);
+	std::cout << "raw: " << raw << std::endl;
+	newPtr = deserialize(raw);
+	std::cout << "newPtr->data = " << newPtr->data << std::endl;
+
+}
