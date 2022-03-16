75d4b97e7381ff7e4d9cdb80bed0df70b7c86be5
Nayeon Kim <naykim@c2r4s1.42seoul.kr>
2022-03-16 21:19:18 +0900

    cpp 07-01 const

--- cpp_module/cpp07/ex01/iter.hpp
+++ cpp_module/cpp07/ex01/iter.hpp
@@ -2,7 +2,7 @@
 #define __ITER_H__
 
 template <typename T>
-void iter(T *array, int length, void (*f)(T &))
+void iter(T *array, int length, void (*f)(T const &))
 {
 	for (int i = 0; i < length; i++)
 		f(array[i]);
