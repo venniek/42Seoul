5e7a29439b536fc9f603296d034fc7a28efdfd3e
Nayeon Kim <naykim@c2r4s1.42seoul.kr>
2022-05-03 20:19:04 +0900

    cont vector 함수 쓰기 시작

---
+++ ft_containers/vector.hpp
@@ -0,0 +1,66 @@
+#ifndef __VECTOR_HPP__
+#ifndef __VECTOR_HPP__
+
+namespace ft {
+    template <typename T, >
+    class vector {
+    private:
+        typedef allocator_type::reference reference;
+        typedef allocator_type::const_reference const_reference;
+    public:
+        explicit vector (const allocator_type& alloc = allocator_type());
+        explicit vector (size_type n, const value_type& val = value_type(),
+                const allocator_type& alloc = allocator_type());
+        template <class InputIterator>
+        vector (InputIterator first, InputIterator last,
+                const allocator_type& alloc = allocator_type());
+        vector (const vector& x);
+        ~vector();
+        vector& operator=(const vector& x);
+    
+    iterator begin();
+    const_iterator begin() const;
+
+    iterator end();
+    const_iterator end() const;
+
+    reverse_iterator rbegin();
+    const_reverse_iterator rbegin() const;
+
+    reverse_iterator rend();
+    const_reverse_iterator rend() const;
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+#endif
