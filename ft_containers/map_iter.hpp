8a9b9df6a75e5e035ce906ebef06bbd2553b4a66
Nayeon Kim <vennie20@naver.com>
2022-06-30 03:17:31 +0900

    cont map_iter 수정

--- ft_containers/map_iter.hpp
+++ ft_containers/map_iter.hpp
@@ -7,189 +7,105 @@
 
 namespace ft
 {
-
-	template<typename Key, typename T, typename Compare = ft::less<Key> >
+	template <typename T, typename Iter>
 	class mapIter
 	{
+	public:
+		typedef T value_type;
+		typedef value_type* pointer;
+		typedef value_type& reference;
+		typedef Iter* iterator_type;
+		typedef typename ft::iterator_traits<iterator_type>::difference_type difference_type;
+		typedef typename ft::iterator_traits<iterator_type>::value_type node_type;
+		typedef typename ft::iterator_traits<iterator_type>::pointer node_pointer;
+		typedef typename ft::iterator_traits<iterator_type>::reference node_reference;
+		typedef typename ft::iterator_traits<iterator_type>::iterator_category iterator_category;
 	private:
-		typedef ft::pair<Key, T> pair_t;
-		typedef Rbtree<Key, T, Compare> tree_type;
-				std::allocator<tree_type> _allocTree;
-		
-				
-
-		typedef tree_type *tree_ptr;
-		typedef Node<pair_t> *RbtNode;
-			std::allocator<Node<pair_t> > _allocNow;
-		typedef ptrdiff_t difference_type;
-
-		tree_ptr _tree;
-		RbtNode _now;
+		node_pointer _now;
+		node_pointer _nil;
 
 	public:
-		class OutOfRangeException: public std::exception
-		{
-		public:
-			const char* what(void) const throw()
-			{
-				return "Iterator is out of range";
-			}
-		};
-
-		mapIter(): _now(NULL) { 
-			tree_type tmp;
-			_tree = _allocTree.allocate(1);
-			_now = _allocNow.allocate(1);
-			*_tree = tmp;
-		}
-		mapIter(tree_ptr src, RbtNode now): _tree(src), _now(now) { }
-		template<typename RmapIter>
-		mapIter(RmapIter &src)
-		{
-			*this = src;
-		}
-		mapIter &operator=(mapIter const &rhs)
-		{
-			if (this != &rhs)
-			{
-				*(this->_tree) = *(rhs._tree);
-				*(this->_now) = *(rhs._now);
-			}
-			return *this;
-		}
+		mapIter(): _now(ft::nil), _nil(ft::nil) { }
+		mapIter(node_pointer now, node_pointer nil): _now(now), _nil(nil) { }
+		mapIter(const mapIter& copy): _now(copy._now), _nil(copy._nil) { }
 		~mapIter() { }
 
-		template<typename U, typename V>
-		bool operator==(const mapIter<U, V, Compare> &rhs) const
+		mapIter& operator=(const mapIter& origin)
 		{
-			return this->_now == rhs._now;
-		}
-		template<typename U, typename V>
-		bool operator!=(const mapIter<U, V, Compare> &rhs) const
-		{
-			return this->_now != rhs._now;
-		}
-
-		mapIter &operator++()
-		{
-			_now = _tree->successor(_now);
-			return *this;
-		}
-		mapIter operator++(int)
-		{
-			mapIter tmp(*this);
-			++(*this);
-			return tmp;
-		}
-		mapIter &operator--()
-		{
-			_now = _tree->predecessor(_now);
-			return *this;
-		}
-		mapIter operator--(int)
-		{
-			mapIter tmp(*this);
-			--(*this);
-			return tmp;
-		}
-	
-		pair_t& operator*() const
-		{
-			return _now->data;
-		}
-		
-		pair_t* operator->() const
-		{
-			return &(this->operator*());
-		}
-		
-		operator mapIter<const Key, T, Compare>() const
-		{
-			return mapIter<const Key, T, Compare>(this->_tree, this->_now);
-		}
-	};
-
-	template<typename Key, typename T, typename Compare = ft::less<Key> >
-	class mapConstIter
-	{
-	private:
-		typedef ft::pair<Key, T> pair_t;
-		typedef Rbtree<Key, T, Compare> tree_type;
-		typedef tree_type *tree_ptr;
-		typedef Node<pair_t> *RbtNode;
-		typedef ptrdiff_t difference_type;
-		typedef pair_t &reference;
-		typedef pair_t *pointer;
-
-		tree_ptr _tree;
-		RbtNode _now;
-
-	public:
-
-		mapConstIter(): _tree(&tree_type()), _now(NULL) { }
-		mapConstIter(tree_ptr src, RbtNode now): _tree(src), _now(now) { }
-		template<typename RmapConstIter>
-		mapConstIter(RmapConstIter &src)
-		{
-			*this = src;
-		}
-		mapConstIter &operator=(mapConstIter const &rhs)
-		{
-			if (this != &rhs)
+			if (this != &origin)
 			{
-				*(this->_tree) = *rhs._tree;
-				*(this->now) = *rhs._now;
+				_now = origin._now;
+				_nil = origin._nil;
 			}
 			return *this;
 		}
-		~mapConstIter() { }
-
-		template<typename U, typename V>
-		bool operator==(const mapConstIter<U, V, Compare> &rhs) const
+		// element access
+		node_pointer base() const
 		{
-			return this->_now == rhs._now;
+			return _now;
 		}
-		template<typename U, typename V>
-		bool operator!=(const mapConstIter<U, V, Compare> &rhs) const
+		pointer operator->() const
 		{
-			return this->_now != rhs._now;
+			return &(_now->data);
 		}
-
-		mapConstIter &operator++()
-		{
-			_now = _tree->successor(_now);
-			return *this;
-		}
-		mapConstIter operator++(int)
-		{
-			mapConstIter tmp(*this);
-			++(*this);
-			return tmp;
-		}
-		mapConstIter &operator--()
-		{
-			_now = _tree->predecessor(_now);
-			return *this;
-		}
-		mapConstIter operator--(int)
-		{
-			mapConstIter tmp(*this);
-			--(*this);
-			return tmp;
-		}
-		const pair_t& operator*() const
+		reference operator*() const
 		{
 			return _now->data;
 		}
-		const pair_t* operator->() const
+		// increment & decrement
+		mapIter& operator++()
 		{
-			return &(this->operator*());
+			_now = successor(_now, _nil);
+			return *this;
 		}
-		operator mapConstIter<const Key, T, Compare>() const
+		mapIter& operator--()
 		{
-			return mapConstIter<const Key, T, Compare>(this->_tree, this->_now);
+			_now = predecessor(_now, _nil);
+			return *this;
+		}
+		mapIter& operator++(int)
+		{
+			mapIter tmp(*this);
+			++(*this);
+			return tmp;
+		}
+		mapIter& operator--(int)
+		{
+			mapIter tmp(*this);
+			--(*this);
+			return tmp;
+		}
+
+		// relational operators
+		template<typename T>
+		bool operator==(const mapIter& rhs) const
+		{
+			return _now == rhs.base();
+		}
+		template<typename T>
+		bool operator!=(const mapIter& rhs) const
+		{
+			return !(*this == rhs);
+		}
+		template<typename T>
+		bool operator==(const mapIter& rhs) const
+		{
+			return _now == rhs.base();
 		}
 	};
+
+	// non member function
+	template<typename T, typename Iter>
+	bool operator==(const mapIter<T, Iter>& x, const mapIter<T, Iter>& y)
+	{
+		return x.base() == y.base();
+	}
+	template<typename T, typename Iter>
+	bool operator!=(const mapIter<T, Iter>& x, const mapIter<T, Iter>& y)
+	{
+		return !(x == y);
+	}
+	
 }
 
 
